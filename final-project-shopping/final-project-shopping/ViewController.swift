f15bebe8dc9072aa5eed695749378b88b8d47c56
sevvaltiraslar <sevvaltiraslar@gmail.com>
2022-11-06 13:21:24 +0300

    Init

---
+++ final-project-shopping/final-project-shopping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  final-project-shopping
+//
+//  Created by SEVVAL on 6.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
